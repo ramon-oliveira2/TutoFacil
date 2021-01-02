18b023456a74fed95c61748184605672598d177d
EMANUEL VASCONCELOS <emanuelvanobre@gmail.com>
2021-01-02 16:35:21 -0300

    atualizando o problema da imagem e mudando o title do php dos artigos(modelo_artigo.php

--- tutoriais_pesquisa.php
+++ tutoriais_pesquisa.php
@@ -8,7 +8,7 @@ include 'conexao.php';
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>TutoFacil - Tutoriais</title>
+  <title>TutoFacil - Tutoriais </title>
   <link rel="stylesheet" type="text/css" href="CSS/style-home.css">
   <link rel="stylesheet" type="text/css" href="CSS/normalize.css">
   <link rel="stylesheet" type="text/css" href="CSS/style-navbar.css">
